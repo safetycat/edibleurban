f440132b41010a798930e31c9572a73fedc63a40
James Cat <james@catplusplus.org.uk>
2015-09-04 13:30:09 +0100

    include content from page in wordpress

--- content/themes/ediblesage/front-page.php
+++ content/themes/ediblesage/front-page.php
@@ -1,3 +1,7 @@
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
+
 <div ng-app="App">
     <div ng-view=""></div>
     <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
