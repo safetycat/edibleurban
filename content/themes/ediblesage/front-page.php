058e11ee5ad6f820978846ae9ee364c69e1be49a
James Cat <james@catplusplus.org.uk>
2015-09-17 11:17:55 +0100

    show/hide intro text

--- content/themes/ediblesage/front-page.php
+++ content/themes/ediblesage/front-page.php
@@ -1,6 +1,28 @@
+<div class="intro-text well">
 <?php while (have_posts()) : the_post(); ?>
     <?php the_content(); ?>
 <?php endwhile; ?>
+<button class="intro-text_close_open button btn-default">
+    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span><span class="text"> Close</span>
+</button>
+</div>
+
+<script type="text/javascript">
+    
+    jQuery(function(){
+        jQuery('.intro-text_close_open').click(function(e){
+            $box = jQuery('.intro-text');
+            console.log($box);
+            if($box.hasClass('minimised')){
+                $box.removeClass('minimised');
+                 $box.find('.text').html(' Close');
+            } else {
+                $box.addClass('minimised');
+                $box.find('.text').html(' Open');
+            }
+        });
+    });
+</script>
 
 <div ng-app="App">
     <div ng-view=""></div>
